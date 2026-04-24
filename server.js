8f82d37640fe5de96b7aadfffdc5bc59990e8665
Gunay Gultekin <gunaygultekin@gmail.com>
2026-04-24 23:27:32 +0100

    Refactor server.js to use require for http module

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import http from "http";
+const http = require("http");
 
 const port = process.env.PORT || 3000;
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("OK\n");
-  })
-  .listen(port, "0.0.0.0", () => {
-    console.log("HTTP wrapper listening on", port);
-  });
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("OK\n");
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log("HTTP wrapper listening on", port);
+});
